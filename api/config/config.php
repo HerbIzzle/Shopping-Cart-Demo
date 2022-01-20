3e6cbf7c390203f5303461e88ec353b3c47da30d
Herbert Knaf <58120773+HerbIzzle@users.noreply.github.com>
2022-01-20 14:11:33 +0100

    config edit

--- api/config/config.php
+++ api/config/config.php
@@ -8,7 +8,7 @@ include "models/productsModel.php";
 include "views/JsonView.php";
 include "models/cartModel.php";
 
-define("DBHost", "gj7ccywfb6bh.eu-west-3.psdb.cloud");
-define("DBName", "shoppingcartdemo");
-define("DBPassword", "pscale_pw_XRpzaBAc1SkPGRVOsHKs-RNE2f6MRhYbmDGRvxkFAno");
-define("DBUsername", "sdweho59vpbc");
+define("DBHost", "localhost");
+define("DBName", "");
+define("DBPassword", "");
+define("DBUsername", "root");
