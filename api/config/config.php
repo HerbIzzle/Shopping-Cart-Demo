48d2651e7371945772475ef6fa0b7d947b64856f
Herbert Knaf <58120773+HerbIzzle@users.noreply.github.com>
2022-01-21 15:03:37 +0100

    heroku config

--- api/config/config.php
+++ api/config/config.php
@@ -8,7 +8,8 @@ include "models/productsModel.php";
 include "views/JsonView.php";
 include "models/cartModel.php";
 
-define("DBHost", "localhost");
-define("DBName", "");
-define("DBPassword", "");
-define("DBUsername", "root");
+$cleardb_url = parse_url(getenv("CLEAR_DATABASE_URL"));
+define("DBHost", $cleardb_url["host"]);
+define("DBName", $cleardb_url["path"]);
+define("DBPassword", $cleardb_url["pass"]);
+define("DBUsername", $cleardb_url["user"]);
